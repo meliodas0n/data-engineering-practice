a2973fc59fbc83da5d6ab21429eee4079225e264
meliodas0n <mrunalnarayana@gmail.com>
2024-11-02 15:00:51 +0530

    exercise 1 complete

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,4 @@
-import requests
+import os, requests, zipfile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -7,14 +7,21 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
-
 def main():
-    # your code here
-    pass
-
+    os.makedirs("downloads") if "downloads" not in os.listdir() else None 
+    for url in download_uris:
+        f_name = url.split("/")[-1]
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(f"downloads/{f_name}", "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        zip_name = f"downloads/{f_name}"
+        with zipfile.ZipFile(zip_name, "r") as z:
+            z.extractall("downloads/")
+        os.remove(zip_name)
 
 if __name__ == "__main__":
     main()
