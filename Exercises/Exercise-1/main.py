c110d761ac78de0a92f3f269f156baed74ad2a9a
meliodas0n <mrunalnarayana@gmail.com>
2024-11-02 18:18:56 +0530

    this is fast

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,5 @@
 import os, requests, zipfile
+from concurrent.futures import ThreadPoolExecutor
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -9,19 +10,20 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
 ]
 
-def main():
-    os.makedirs("downloads") if "downloads" not in os.listdir() else None 
-    for url in download_uris:
-        f_name = url.split("/")[-1]
-        with requests.get(url, stream=True) as r:
-            r.raise_for_status()
-            with open(f"downloads/{f_name}", "wb") as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-        zip_name = f"downloads/{f_name}"
-        with zipfile.ZipFile(zip_name, "r") as z:
-            z.extractall("downloads/")
-        os.remove(zip_name)
+def download_extract(url):
+    f_name = url.split("/")[-1]
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(f"downloads/{f_name}", "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    zipfile.ZipFile(f"downloads/{f_name}", "r").extractall("data/")
+    os.remove(f"downloads/{f_name}")
 
+def main():
+    os.makedirs("downloads") if "downloads" not in os.listdir() else None
+    with ThreadPoolExecutor(max_workers=len(download_uris)) as exe:
+        exe.map(download_extract, download_uris)
+        
 if __name__ == "__main__":
     main()
