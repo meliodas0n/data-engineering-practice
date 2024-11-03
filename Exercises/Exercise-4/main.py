35829c479e366ac79ddf63511bafebf175b3a519
meliodas0n <mrunalnarayana@gmail.com>
2024-11-03 18:20:59 +0000

    should be on right track

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,7 +1,12 @@
-import blob, json, pandas as pd
+import os, glob, json, pandas as pd
 
 def main():
-    pass
+    DATA = []
+    jfiles = glob.glob("data/**/*.json", recursive=True)
+    for i, jf in enumerate(jfiles):
+        if i == 0: df = pd.read_json(jf)
+        else: df = pd.concat([df, pd.read_json(jf)])
+    print(df.head())
 
 if __name__ == "__main__":
     main()
