3dc808f1b512c3b1b148948da437c73cab513275
meliodas0n <mrunalnarayana@gmail.com>
2024-11-03 18:44:49 +0000

    this migtt work

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -3,10 +3,14 @@ import os, glob, json, pandas as pd
 def main():
     DATA = []
     jfiles = glob.glob("data/**/*.json", recursive=True)
-    for i, jf in enumerate(jfiles):
-        if i == 0: df = pd.read_json(jf)
-        else: df = pd.concat([df, pd.read_json(jf)])
-    print(df.head())
+    for jd in jfiles:
+        with open(jd, "r") as d:
+            DATA.append(json.load(d))
+        # if i == 0: df = pd.read_json(jf)
+        # else: df = pd.concat([df, pd.read_json(jf)])
+    # df = df.explode(['type', 'Point'])
+    # df.to_csv("data.csv", index=False)
+    print(DATA)
 
 if __name__ == "__main__":
     main()
